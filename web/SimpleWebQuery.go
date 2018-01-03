fc49380383b5a06b75deea491f94002ff53ee45c
Aakash Goyal <aakash.gyl@gmail.com>
2018-01-03 17:33:43 +0530

    Update SimpleWebQuery.go

--- web/SimpleWebQuery.go
+++ web/SimpleWebQuery.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	response, _ := http.Get("http://10.71.11.188:8080/view/ANewPage")
-
+	url := "http://10.71.11.188:8080/view/ANewPage"
+	
+	response, _ := http.Get(url)
 	defer response.Body.Close()
+	
 	body, _ := ioutil.ReadAll(response.Body)
 
 	fmt.Println(response)
 	fmt.Println(string(body))
-
 }
